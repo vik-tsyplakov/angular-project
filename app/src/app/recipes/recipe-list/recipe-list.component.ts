3eb268d1f748f2362059763cbb395266b57d2b93
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-02-19 23:19:41 +0400

    added-recipe-card

--- app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
-  recipes = [];
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simple test',
+      'https://img.delicious.com.au/Pwnp-j1O/w1200/del/2022/08/ramenara-ramen-carbonara-172843-2.png'
+    ),
+  ];
+  constructor() {}
+  ngOnInit() {}
 }
