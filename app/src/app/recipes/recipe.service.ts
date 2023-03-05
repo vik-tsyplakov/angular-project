4db4cacd0a76a5b7a7585f1e41e79133b10346a8
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-03-05 20:18:28 +0400

    added-ingredients-at-recipe-detail

--- app/src/app/recipes/recipe.service.ts
+++ app/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,14 +7,30 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
-      'This is simple test',
-      'https://img.delicious.com.au/Pwnp-j1O/w1200/del/2022/08/ramenara-ramen-carbonara-172843-2.png'
+      'Spaghetti carbonara recipe',
+      'Discover how to make superb spaghetti carbonara. This cheesy pasta dish is an Italian favourite and with the right technique, you can make it perfect every time',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg',
+      [
+        new Ingredient('pancetta (grams)', 100),
+        new Ingredient('pecorino cheese (grams)', 50),
+        new Ingredient('parmesan (grams)', 50),
+        new Ingredient('large eggs', 3),
+        new Ingredient('spaghetti (grams)', 350),
+        new Ingredient('plump garlic cloves, peeled and left whole', 2),
+        new Ingredient('unsalted butter (grams)', 50),
+        new Ingredient('sea salt and freshly ground black pepper (pinch)', 1),
+      ]
     ),
     new Recipe(
-      'Another test Recipe',
-      'This is simple test',
-      'https://img.delicious.com.au/Pwnp-j1O/w1200/del/2022/08/ramenara-ramen-carbonara-172843-2.png'
+      'Teriyaki pork meatballs',
+      'Just four supermarket ingredients stand between you and this super quick, low-calorie supper. Meatballs in a teriyaki sauce with fresh and crunchy pak choi',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/teriyaki-noodles-e167565.jpg',
+      [
+        new Ingredient('dried medium egg noodles (grams)', 250),
+        new Ingredient('pork meatballs', 12),
+        new Ingredient('pak choi (grams)', 250),
+        new Ingredient('tbsp teriyaki sauce', 6),
+      ]
     ),
   ];
 
