a9796fa55d9fd3ebe10dd028d50e7817bac915e3
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-03-07 21:46:36 +0400

    added-routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
